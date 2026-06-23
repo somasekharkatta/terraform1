186a6bc79905dff241137bc58b0e6deed4753598
somasekharkatta <130534074+somasekharkatta@users.noreply.github.com>
2026-06-24 00:03:37 +0530

    Delete module/ec2-instance/varibles.tf

--- module/ec2-instance/varibles.tf
+++
@@ -1,9 +0,0 @@
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-}
