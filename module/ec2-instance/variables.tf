438e7ef21fc9c67a80752c4406a7e6ca33bd7fdd
somasekharkatta <130534074+somasekharkatta@users.noreply.github.com>
2026-06-24 00:04:13 +0530

    Update variables.tf

--- module/ec2-instance/variables.tf
+++ module/ec2-instance/variables.tf
@@ -1,9 +1,7 @@
 variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
+  default = "ami-01a00762f46d584a1"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
+  default = "t2.small"
 }
